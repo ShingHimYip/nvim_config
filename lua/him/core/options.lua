f19313903d5fd06be959d8eb4f804e65e8191667
Shing Him Yip <shing.yip.2020@live.rhul.ac.uk>
2023-06-24 00:52:41 +0100

    make it work for window

--- lua/him/core/options.lua
+++ lua/him/core/options.lua
@@ -1,3 +1,19 @@
+function os_based_settings()
+	-- get operating system name
+	local os_name = vim.loop.os_uname().sysname
+
+	-- check if OS is Windows
+	if os_name == "Windows_NT" then
+		-- Windows-specific settings
+		vim.opt.undodir = os.getenv("USERPROFILE") .. "\\.vim\\undodir"
+	else
+		-- Linux and MacOS settings
+		vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+	end
+end
+
+os_based_settings()
+
 local opt = vim.opt
 
 opt.relativenumber = true -- show relative line numbers
@@ -12,7 +28,6 @@ opt.wrap = false
 
 opt.swapfile = false
 opt.backup = false
-opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 opt.undofile = true
 
 opt.ignorecase = true
@@ -24,31 +39,26 @@ opt.smartcase = true -- if you include mixed case in your search, assumes you wa
 opt.cursorline = true -- highlight the current cursor line
 
 -- appearance
-
--- turn on termguicolors for nightfly colorscheme to work
--- (have to use iterm2 or any other true color terminal)
 opt.termguicolors = true
-opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+opt.background = "dark"
+opt.signcolumn = "yes"
 opt.scrolloff = 8
 opt.updatetime = 50
 opt.colorcolumn = "120"
 
 -- backspace
-opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+opt.backspace = "indent,eol,start"
 
 -- clipboard
--- opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+-- opt.clipboard:append("unnamedplus")
 
 -- split windows
-opt.splitright = true -- split vertical window to the right
-opt.splitbelow = true -- split horizontal window to the bottom
+opt.splitright = true
+opt.splitbelow = true
 
-opt.iskeyword:append("-") -- consider string-string as whole word
+opt.iskeyword:append("-")
 
 -- spellcheck
 -- opt.spell = true
--- opt.spelllang = "en_gb,cjk" -- use cjk to prevent Asian words from flagging up as errors
+-- opt.spelllang = "en_gb,cjk"
 -- opt.spelloptions = "camel"
-
--- print('run in option')
