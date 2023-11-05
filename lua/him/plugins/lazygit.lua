056d902807ecf4b75b256bf005f5020e7ff34518
Shing Him Yip <shy@redwoodtech.com>
2023-11-05 16:15:09 +0000

    added some doc for the lazygit conflict

--- lua/him/plugins/lazygit.lua
+++ lua/him/plugins/lazygit.lua
@@ -4,3 +4,23 @@ if not setup then
 end
 
 vim.keymap.set("n", "<leader>lg", "<cmd>LazyGit<CR>")
+
+-- NOTE: The following is the setting that u need to put into the .gitconfig
+
+--[merge]
+--   tool = nvim
+-- [mergetool "nvim"]
+--   cmd = nvim -f -c "Gvdiffsplit!" "$MERGED"
+-- [mergetool]
+--   prompt = false
+
+-- NOTE: The following is the config.yml for the lazygit
+
+--os:
+--   editPreset: "nvim-remote"
+--   edit: "nvim {{filename}}"
+--
+-- git:
+--   merging:
+--     manualMergeCommand: 'nvim -f -c "Gvdiffsplit!" "$MERGED"'
+--
